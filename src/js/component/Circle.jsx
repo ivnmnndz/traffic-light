94dc0da68922cf4e65efd63791cc49b77ee3c6bc
ivnmnndz <12gues@gmail.com>
2021-10-18 23:47:04 +0000

    formatting and removed console.logs

--- src/js/component/Circle.jsx
+++ src/js/component/Circle.jsx
@@ -2,21 +2,17 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Circle(props) {
-	return (
-		<div
-			onClick={props.onClick}
-			className={`
-      circle 
-
-      bg-${props.lights.bg} 
-
-      ${props.selected ? "glow" : ""}`}></div>
-	);
+  return (
+    <div
+      onClick={props.onClick}
+      className={`circle bg-${props.lights.bg} ${props.selected ? "glow" : ""}`}
+    ></div>
+  );
 }
 
 Circle.propTypes = {
-	lights: PropTypes.object,
-	selected: PropTypes.bool,
-	onClick: PropTypes.func
+  lights: PropTypes.object,
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
 };
 export default Circle;
--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -4,7 +4,6 @@ import lights from "../data.js";
 
 const TrafficLight = () => {
 	const [isSelected, setIsSelected] = useState("");
-	console.log("line 7 isSelected: ", isSelected);
 	return (
 		<div className="traffic-light">
 			{lights.map((item, index) => (
@@ -14,7 +13,6 @@ const TrafficLight = () => {
 					selected={lights[index].color === isSelected ? true : false}
 					onClick={() => {
 						setIsSelected(lights[index].color);
-						console.log("line 17 isSelected: ", isSelected);
 					}}
 				/>
 			))}
